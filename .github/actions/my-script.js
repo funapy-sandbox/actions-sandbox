91e72032d50e65ae0253969866e2d60a5783f249
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-06-13 23:22:01 +0900

    fix

--- .github/actions/my-script.js
+++ .github/actions/my-script.js
@@ -3,8 +3,16 @@ const { Octokit } = require("@octokit/action");
 (async () => {
   const octokit = new Octokit();
   const sha = process.env.SHA;
+ 
+  console.log({
+    owner: 'funapy-sandbox',
+    repo: 'docs',
+    sha: sha,
+    state: "success"
+  });
+  
   // See https://developer.github.com/v3/issues/#create-an-issue
-  const { data } = octokit.request('POST /repos/{owner}/{repo}/statuses/{sha}', {
+  const { data } = await octokit.request('POST https://api.github.com/repos/{owner}/{repo}/statuses/{sha}', {
     owner: 'funapy-sandbox',
     repo: 'docs',
     sha: sha,
